4d5b34f63594ef5efc309cd0ffb97081e632eee1
Nikhil Varma <mantenanikhil007@gmail.com>
2024-11-27 15:14:40 -0500

    S3 service script

--- config.py
+++ config.py
@@ -7,23 +7,45 @@ aws_access_key_id:str = getpass.getpass('AWS_ACCESS_KEY_ID: ')
 aws_secret_access_key:str = getpass.getpass('AWS_SECRET_ACCESS_KEY: ')
 
 '''
-MAIN MENU - 1
+MAIN MENU 
 '''
 def dashboard_init():
     print('Welcome to the Dashboard...\n')
-    main_menu_selection = questionary.select(
+    while True:
+        main_menu_selection = questionary.select(
         'Choose from the below services',
-        choices=['S3','Kinesis-Datastreams','Kinesis-Firehosestreams']
-    ).ask()
-    match main_menu_selection:
-        case 'S3':
-            s3_menu()
+        choices=['S3','Kinesis-Datastreams','Kinesis-Firehosestreams','Exit']
+        ).ask()
+
+        match main_menu_selection:
+            case 'S3':
+                s3_menu()
+            case 'Kinesis-Datastreams':
+                kinesis_datastream_menu()
+            case 'Kinesis-Firehosestreams':
+                kinesis_datafirehose_menu()
+            case 'Exit':
+                print(Fore.RED,'Script terminated.')
+                exit()
+            
  
 '''
-S3 MENU - 2
+S3 Menu - 1
 '''
 def s3_menu()->None:
-    print('S3 OPTIONS')
+    print(Fore.YELLOW,'Choose from the below ')
+
+'''
+Kineis Data Streams Menu - 2
+'''
+def kinesis_datastream_menu()->None:
+    print(Fore.YELLOW,'Choose from Below Kinesis DataStream Options: ')
+
+'''
+Kinesis Data Firehose Streams - 3
+'''
+def kinesis_datafirehose_menu()->None:
+    print(Fore.YELLOW,'Choose form below Firehose Options')
 
 
 def main()->None:
